975ed69593ffbb38317a29c05571ab8e45fcb08b
joe18B <joe.hardy@18Birdies.com>
2016-07-26 12:44:22 -0400

    Added LocationManager class files

---
+++ SmartHouseTab/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  SmartHouseTab
+//
+//  Created by Joseph Hardy on 7/26/16.
+//  Copyright © 2016 Joseph Hardy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ SmartHouseTab/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  SmartHouseTab
+//
+//  Created by Joseph Hardy on 7/26/16.
+//  Copyright © 2016 Joseph Hardy. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
